0c99627d48432b4aa9320be918fd3313575ccfd4
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-19 22:13:53 +0530

    a

--- Angular5-Seed/src/app/app.module.ts
+++ Angular5-Seed/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { ProductComponent } from './products/products.component';
+import { UpperPipe } from './products/myUpper.pipe';
 
 @NgModule({
     // All Component come here
     declarations: [
         AppComponent,
         DashboardComponent,
-        ProductComponent
+        ProductComponent,
+        UpperPipe
      ],
      // All Modules will declare here
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     // Only main component
     bootstrap: [
