f93a1f41ca5a3f180f41fef2ae98b8f289870192
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-23 21:43:49 +0530

    a

--- Angular5-Seed/src/app/app.module.ts
+++ Angular5-Seed/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -8,6 +9,8 @@ import { ProductComponent } from './products/products.component';
 import { UpperPipe } from './products/myUpper.pipe';
 import { PricePipe } from './products/myPrice.pipe';
 import { ProductValueFilter } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductService } from './products/product.service';
 
 @NgModule({
     // All Component come here
@@ -17,19 +20,23 @@ import { ProductValueFilter } from './products/product-filter.pipe';
         ProductComponent,
         UpperPipe,
         PricePipe,
-        ProductValueFilter
+        ProductValueFilter,
+        StarComponent
      ],
      // All Modules will declare here
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        HttpModule
     ],
     // Only main component
     bootstrap: [
         AppComponent
      ],
      // Services will declare here
-    providers: [ ]
+    providers: [
+        ProductService
+    ]
 })
 
 export class AppModule {
